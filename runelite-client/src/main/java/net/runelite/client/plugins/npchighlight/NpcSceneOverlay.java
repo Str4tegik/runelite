03a90af31f89da59946e6702d4f30f8d8263d8f7
Adam <Adam@sigterm.info>
2020-06-04 22:11:31 -0400

    Revert "npc indicators: add option to not highlight dead npcs"
    This reverts commit 869b1ba1d9c3c1f67e0bf9471750b1fd3082d82e.

--- runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcSceneOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/npchighlight/NpcSceneOverlay.java
@@ -146,8 +146,7 @@ public class NpcSceneOverlay extends Overlay
 	private void renderNpcOverlay(Graphics2D graphics, NPC actor, Color color)
 	{
 		NPCComposition npcComposition = actor.getTransformedComposition();
-		if (npcComposition == null || !npcComposition.isInteractible()
-			|| (actor.isDead() && !config.highlightDeadNpcs()))
+		if (npcComposition == null || !npcComposition.isInteractible())
 		{
 			return;
 		}
